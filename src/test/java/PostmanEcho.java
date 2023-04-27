b2c17f7ba8fc46d1c1a52ff5ccfc2905727e3ea5
mvalerko <04445326@mail.ru>
2023-04-27 21:16:33 +0200

    Тест падает

---
+++ src/test/java/PostmanEcho.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEcho {
+    @Test
+    void Post() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello World!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("No"));
+
+    }
+}
