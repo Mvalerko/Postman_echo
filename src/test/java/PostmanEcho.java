bb65ec0f45db5f3b2076a1f9cd5a8f43daa6cd2d
mvalerko <04445326@mail.ru>
2023-04-27 21:53:40 +0200

    Тест проходит

--- src/test/java/PostmanEcho.java
+++ src/test/java/PostmanEcho.java
@@ -21,7 +21,7 @@ class PostmanEcho {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("No"));
+                .body("data", equalTo("Hello World!"));
 
     }
 }
